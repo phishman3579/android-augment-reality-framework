7aa074e56b0d1e61d23f3826ffcfbfff26b81ee6
phishman3579@gmail.com <phishman3579@gmail.com@62fff5c4-409e-9657-2154-d1f60553b75b>
2011-11-24 17:04:32 +0000

    Finally fixed the touch detection
    git-svn-id: https://android-augment-reality-framework.googlecode.com/svn/trunk@65 62fff5c4-409e-9657-2154-d1f60553b75b

--- src/com/jwetherell/augmented_reality/ui/Marker.java
+++ src/com/jwetherell/augmented_reality/ui/Marker.java
@@ -7,6 +7,7 @@ import com.jwetherell.augmented_reality.common.Utilities;
 import com.jwetherell.augmented_reality.common.Vector;
 import com.jwetherell.augmented_reality.data.ARData;
 import com.jwetherell.augmented_reality.data.PhysicalLocation;
+import com.jwetherell.augmented_reality.ui.objects.PaintableBox;
 import com.jwetherell.augmented_reality.ui.objects.PaintableBoxedText;
 import com.jwetherell.augmented_reality.ui.objects.PaintableGps;
 import com.jwetherell.augmented_reality.ui.objects.PaintableObject;
@@ -15,6 +16,7 @@ import com.jwetherell.augmented_reality.ui.objects.PaintablePosition;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.location.Location;
+import android.util.Log;
 
 
 /**
@@ -70,6 +72,11 @@ public class Marker implements Comparable<Marker> {
     //Marker's default color
     protected int color = Color.WHITE;
 
+    //Used to debug the touching mechanism
+    private static boolean debugTouchZone = false;
+    private static PaintableBox touchBox = null;
+    private static PaintablePosition touchPosition = null;
+    
 	public Marker(String name, double latitude, double longitude, double altitude, int color) {
 		set(name, latitude, longitude, altitude, color);
 	}
@@ -167,12 +174,12 @@ public class Marker implements Comparable<Marker> {
     }
 
     public synchronized float getHeight() {
-        if (textContainer==null) return 0f;
+        if (symbolContainer==null || textContainer==null) return 0f;
         return symbolContainer.getHeight()+textContainer.getHeight();
     }
     
     public synchronized float getWidth() {
-        if (textContainer==null) return 0f;
+        if (symbolContainer==null || textContainer==null) return 0f;
         float w1 = textContainer.getWidth();
         float w2 = symbolContainer.getWidth();
         return (w1>w2)?w1:w2;
@@ -329,8 +336,6 @@ public class Marker implements Comparable<Marker> {
      * @return True if the point is on Marker.
      */
 	public synchronized boolean isPointOnMarker(float x, float y) {
-	    if (symbolContainer==null) return false;
-	    
 	    symbolXyzRelativeToCameraView.get(symbolArray);
         textXyzRelativeToCameraView.get(textArray);        
         float x1 = symbolArray[0];
@@ -339,12 +344,16 @@ public class Marker implements Comparable<Marker> {
         float y2 = textArray[1];
         float adjX = (x1 + x2)/2;
         float adjY = (y1 + y2)/2;
-        float adjW = (getWidth()/2);
-        float adjH = (getHeight()/2);
-        adjY += (adjH - 25); // A bit of a fudge factor to account for the distance between text and symbol
-        
-        if (x>=(adjX-adjW) && x<=(adjX+adjW) && y>=(adjY-adjH) && y<=(adjY+adjH)) 
+        float width = getWidth();
+        float height = getHeight();
+        float adjWidth = width/2;
+        float adjHeight = height/2;
+        // If the marker has been visible, use the text box to offset the position.
+        if (textBox!=null) adjY += (textBox.getHeight()/2);
+
+        if (x>=(adjX-adjWidth) && x<=(adjX+adjWidth) && y>=(adjY-adjHeight) && y<=(adjY+adjHeight)) 
             return true;
+        
         return false;
 	}
 
@@ -363,8 +372,41 @@ public class Marker implements Comparable<Marker> {
         if (!isOnRadar || !isInView) return;
         
         //Draw the Icon and Text
+        if (debugTouchZone) drawTouchZone(canvas);
         drawIcon(canvas);
         drawText(canvas);
+    }
+
+    protected synchronized void drawTouchZone(Canvas canvas) {
+        if (canvas==null) throw new NullPointerException();
+        
+        if (gpsSymbol==null) return;
+        
+        symbolXyzRelativeToCameraView.get(symbolArray);
+        textXyzRelativeToCameraView.get(textArray);        
+        float x1 = symbolArray[0];
+        float y1 = symbolArray[1];
+        float x2 = textArray[0];
+        float y2 = textArray[1];
+        float width = getWidth();
+        float height = getHeight();
+        float adjX = (x1 + x2)/2;
+        float adjY = (y1 + y2)/2;
+        float currentAngle = Utilities.getAngle(symbolArray[0], symbolArray[1], textArray[0], textArray[1])+90;
+        adjX -= (width/2);
+        adjY -= (gpsSymbol.getHeight()/2);
+        
+        Log.w("touchBox", "ul (x="+(adjX)+" y="+(adjY)+")");
+        Log.w("touchBox", "ur (x="+(adjX+width)+" y="+(adjY)+")");
+        Log.w("touchBox", "ll (x="+(adjX)+" y="+(adjY+height)+")");
+        Log.w("touchBox", "lr (x="+(adjX+width)+" y="+(adjY+height)+")");
+        
+        if (touchBox==null) touchBox = new PaintableBox(width,height);
+        else touchBox.set(width,height);
+
+        if (touchPosition==null) touchPosition = new PaintablePosition(touchBox, adjX, adjY, currentAngle, 1);
+        else touchPosition.set(touchBox, adjX, adjY, currentAngle, 1);
+        touchPosition.paint(canvas);
     }
     
     protected synchronized void drawIcon(Canvas canvas) {
