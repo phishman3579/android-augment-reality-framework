82a6b72b31072f964ee9c0abd969d81a48d76f1d
phishman3579@gmail.com <phishman3579@gmail.com@62fff5c4-409e-9657-2154-d1f60553b75b>
2011-11-25 00:26:16 +0000

    Updated the javadocs to reference online content
    git-svn-id: https://android-augment-reality-framework.googlecode.com/svn/trunk@73 62fff5c4-409e-9657-2154-d1f60553b75b

--- create_javadocs.sh
+++ create_javadocs.sh
@@ -1,3 +1,3 @@
 #1/bin/bash
 
-javadoc -classpath /usr/local/android-sdk-linux_86/platforms/android-4/android.jar -d javadoc/ -sourcepath `find . -name *.java` -verbose com.jwetherell.augmented_reality
+javadoc -author -linkoffline http://d.android.com/reference file:/usr/local/android-sdk-linux_86/docs/reference -classpath /usr/local/android-sdk-linux_86/platforms/android-4/android.jar -d javadoc/ -sourcepath `find . -name *.java`
